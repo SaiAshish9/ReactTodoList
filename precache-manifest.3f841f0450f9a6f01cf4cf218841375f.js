edb6407fcd9a1911f8b3a8e594d45fce11886600
SaiAshish9 <saiashish3760@gmail.com>
2019-07-19 13:12:45 +0530

    Updates

---
+++ precache-manifest.3f841f0450f9a6f01cf4cf218841375f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a790b34d02dca39991b2b97c2bae86",
+    "url": "/ReactTodolist/index.html"
+  },
+  {
+    "revision": "a486fa9202cfcff39f49",
+    "url": "/ReactTodolist/static/css/main.f200c880.chunk.css"
+  },
+  {
+    "revision": "f01d05711bcbb7d90188",
+    "url": "/ReactTodolist/static/js/2.2520225e.chunk.js"
+  },
+  {
+    "revision": "a486fa9202cfcff39f49",
+    "url": "/ReactTodolist/static/js/main.7f9cf369.chunk.js"
+  },
+  {
+    "revision": "554125dc678b3a15ea57",
+    "url": "/ReactTodolist/static/js/runtime~main.06062bc0.js"
+  }
+]);
